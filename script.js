9cc4172af7da43260689f037d65a4904fec5cede
Nihita Lele <124602601+NihitaLele@users.noreply.github.com>
2024-03-01 16:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function firstWord(s) {
-	let words = s;
-   if (words.trim() === '') {
-    return words;
-  }
-  words = words.split(" ");
-  return words[0];
-}	
-
+    if (s === '') {
+        return s;
+    }
+    let words = s.split(" ");
+    return words[0];
+}
+	
 // Do not change the code below
-const s = prompt("Enter String:");
+ const s = prompt("Enter String:");
 alert(firstWord(s));
